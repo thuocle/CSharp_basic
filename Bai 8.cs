aefb3906b1bc379ca34745e71a32455f6ab504ef
Lê Quốc Thước <102453854+thuocle@users.noreply.github.com>
2023-06-16 14:36:10 +0700

    Tính tích số các số trong 1 số nguyên

---
+++ Bai 8.cs
@@ -0,0 +1,13 @@
+ //CS01-6-07: Tính tích số các số trong 1 số nguyên
+            *//*Console.WriteLine("Nhap vao so n");
+            int n = int.Parse(Console.ReadLine());
+            double tich = 1;
+            int tmp = n;
+            int du = 0;
+            while(tmp != 0)
+            {
+                du = tmp % 10;
+                tich = du * tich;
+                tmp /= 10;
+            }
+            Console.WriteLine(tich);*//*
