8b873858b903108b6ae6feeca032f5da47ec459a
Lê Quốc Thước <102453854+thuocle@users.noreply.github.com>
2023-06-16 14:34:55 +0700

    Kiểm tra số nguyên tố

---
+++ Bai 7.cs
@@ -0,0 +1,22 @@
+  //CS01-6-09: Kiểm tra số nguyên tố
+
+            *//*Console.WriteLine("Nhap vao so n: ");
+            int n = int.Parse(Console.ReadLine());
+            bool check = true;
+            if(n < 2)
+            {
+                check = false;   
+            }
+            for(int i = 2; i <= n / 2; i++)
+            {
+                if (n % i == 0)
+                {
+                    check = false;
+                    break;
+                }
+            }
+            if(check == true)
+            {
+                Console.WriteLine($"{n} la so nguyen to");
+            }
+            else { Console.WriteLine($"{n} khong la so nguyen to"); }*//*
