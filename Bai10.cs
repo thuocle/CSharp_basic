175665c81811dd72a6287f288a413b78a8fb1ede
Lê Quốc Thước <102453854+thuocle@users.noreply.github.com>
2023-06-17 10:25:08 +0700

    Viết chương trình trả về vị trí xuất hiện đầu tiên và cuối cùng của ký tự

---
+++ Bai10.cs
@@ -0,0 +1,23 @@
+//CS1_7_06: Viết chương trình trả về vị trí xuất hiện đầu tiên và cuối cùng của ký tự
+            *//*Console.WriteLine("Nhap vao chuoi s: ");
+            string s = Console.ReadLine();
+            Console.WriteLine("Nhap ky tu can tim: ");
+            char c = Console.ReadLine()[0];
+            char[] charArr = s.ToCharArray();
+          
+            for (int i = 0; i < charArr.Length; i++)
+            {
+                if(charArr[i] == c)
+                {
+                    Console.WriteLine($"Ky tu {c} xuat hien lan dau tien tai vi tri {i}");
+                    break;
+                }
+            }
+            for (int i = charArr.Length - 1; i >= 0; i--)
+            {
+                if (charArr[i] == c)
+                {
+                    Console.WriteLine($"Ky tu {c} xuat hien lan cuoi tai vi tri {i}");
+                    break;
+                }
+            }*//*
