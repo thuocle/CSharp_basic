c7a0b6cd0bd5b0ff41cecb39116ee274b3466824
Lê Quốc Thước <102453854+thuocle@users.noreply.github.com>
2023-06-17 10:27:57 +0700

    Nhập vào họ và tên ở dạng chưa chuẩn hóa, hiển thị email có dạng yourname@hvitclan.com

---
+++ Bai14.cs
@@ -0,0 +1,9 @@
+/*Nhập vào họ và tên ở dạng chưa chuẩn hóa, hiển thị email có dạng yourname@hvitclan.com
+            Ví dụ:
+            Đầu vào:  “   Nguyen    DuC TOAN    ” 
+            Đầu ra: nguyenductoan@hvitclan.com*/
+
+            /*string s = "   Nguyen    DuC TOAN   ";
+            string news = s.Trim().Replace(" ","").ToLower();
+            string email = string.Concat(news, "@hvitclan.com");
+            Console.WriteLine(email);*//*
