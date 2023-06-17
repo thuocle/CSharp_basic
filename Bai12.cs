c44fb15d50f99a48980145b518e05c69315a1a9c
Lê Quốc Thước <102453854+thuocle@users.noreply.github.com>
2023-06-17 10:26:59 +0700

    Viết hàm in chuỗi đảo ngược

---
+++ Bai12.cs
@@ -0,0 +1,40 @@
+ //CS1_7_12: Viết hàm in chuỗi đảo ngược
+            *//*Đầu vào: 1 chuỗi bất kì
+            Đầu ra: dạng viết hoa, viết ngược và dạng chuỗi ngược viết hoa của chuỗi ban đầu. 
+            Và đếm xem, có bao nhiêu chữ cái, chữ số và kí tự đặc biệt*/
+
+            /*Console.WriteLine("Nhap chuoi:");
+            string s = Console.ReadLine();
+            string sHoa = s.ToUpper();
+            int cs = 0, cc = 0, kt = 0;
+            char[] charArr = s.ToCharArray();
+            Console.Write($"Chuoi viet hoa: {sHoa}");
+            Console.WriteLine();
+            Console.Write($"Chuoi viet nguoc: ");
+            for (int i = charArr.Length-1; i >= 0 ; i--)
+            {
+                Console.Write(charArr[i]);
+            }
+            Console.WriteLine();
+            Console.Write($"Chuoi nguoc Viet Hoa: ");
+            for (int i = charArr.Length - 1; i >= 0; i--)
+            {
+                Console.Write(charArr[i].ToString().ToUpper());
+            }
+
+            foreach (char item in charArr)
+            {
+                if (char.IsDigit(item))
+                {
+                    cs++;
+                }else if (char.IsLetter(item))
+                {
+                    cc++;
+                }
+                else
+                {
+                    kt++;
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine($"So chu so la {cs}, so chu cai la {cc}, so ky tu dac biet la {kt}");*/
